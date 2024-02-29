10b3a66537852dfa961e94a4062fc62bbb86e7c4
Roy Hadad <39004075+royhadad@users.noreply.github.com>
2024-02-29 23:34:10 +0800

    feat: create quote generator page (react-query + quotable) (#4)
    * feat: create quote generator page with pseudo request
    
    * feat: quote generator working
    
    * style: fix prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import { ProgressBarPage } from "./pages/ProgressBarPage/ProgressBarPage.tsx";
 import { useState } from "react";
+import { QuoteGeneratorPage } from "./pages/QuoteGeneratorPage/QuoteGeneratorPage.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const pages = [
   { jsx: <ProgressBarPage />, name: "Progress Bar" },
+  {
+    jsx: <QuoteGeneratorPage />,
+    name: "Quote generator",
+  },
   {
     jsx: <div className="text-xl">Page 2</div>,
     name: "Page 2",
@@ -18,27 +25,32 @@ const pages = [
   },
 ];
 
+const queryClient = new QueryClient();
+
 function App() {
   const [selectedPageIndex, setSelectedPageIndex] = useState<number>(0);
 
   return (
-    <div className="h-full">
-      <h1>This is my React playground!</h1>
-      <div className="flex-row justify-start align-middle">
-        {pages.map((page, index) => {
-          return (
-            <button
-              key={index}
-              onClick={() => setSelectedPageIndex(index)}
-              className={`mr-2 rounded px-4 py-2 font-bold text-white ${selectedPageIndex === index ? "bg-blue-500" : "bg-blue-300"}`}
-            >
-              {page.name}
-            </button>
-          );
-        })}
+    <QueryClientProvider client={queryClient}>
+      <div className="h-full">
+        <h1>This is my React playground!</h1>
+        <div className="flex-row justify-start align-middle">
+          {pages.map((page, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => setSelectedPageIndex(index)}
+                className={`mr-2 rounded px-4 py-2 font-bold text-white ${selectedPageIndex === index ? "bg-blue-500" : "bg-blue-300"}`}
+              >
+                {page.name}
+              </button>
+            );
+          })}
+        </div>
+        <div>{pages[selectedPageIndex].jsx}</div>
       </div>
-      <div>{pages[selectedPageIndex].jsx}</div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  // ...
 };
